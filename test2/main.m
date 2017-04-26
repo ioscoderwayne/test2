bd8bb76d9d7f103735ee29e682bca8d7f6cf2d04
魏晓阳 <weixiaoyang@meituan.com>
2017-04-26 17:28:30 +0800

    初始化项目

---
+++ test2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test2
+//
+//  Created by weixiaoyang on 2017/4/26.
+//  Copyright © 2017年 weixiaoyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
